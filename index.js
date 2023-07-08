60be09d2f7ff603670c66519c730c741e3df6e0d
Elvis Njau <elvisbater@gmail.com>
2023-07-08 11:26:30 -0300

    sent GET request to retrieve film data

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ let films;
 
 // Function to fetch film data from API endpoint
 async function fetchFilms() {
+    // try-catch block to handle any errors that may occur while fetching data
+  try {
+    // send GET request to 'http://localhost:3000/films' to retrieve film data
+    const response = await fetch('http://localhost:3000/films');
+    films = await response.json();
+    displayFilmList();
+    displayFilmInfo(films[0].id);
+  } catch (error) {
+    console.error(error);
+  }
+}
