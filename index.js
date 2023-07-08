08eedfbb3b338682382ffc2fa6b8adca2a3b4590
Elvis Njau <elvisbater@gmail.com>
2023-07-08 11:23:05 -0300

    added function to fetch film data from API

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const filmListContainer = document.getElementById('filmListContainer');
 const posterCard = document.getElementById('posterCard');
 const infoCard = document.getElementById('infoCard');
+
+
+let films;
+
+// Function to fetch film data from API endpoint
+async function fetchFilms() {
