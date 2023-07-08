45456792570134c78a42b72919fcac0e449b9bdc
Elvis Njau <elvisbater@gmail.com>
2023-07-08 11:27:52 -0300

    added function to create and display list items

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ async function fetchFilms() {
     console.error(error);
   }
 }
+// Function to create and display film list items
+function displayFilmList() {
+    films.forEach((film) => {
+      const filmItem = document.createElement('div');
+      filmItem.innerHTML = film.title;
+      filmItem.classList.add('film-item');
+      filmItem.setAttribute('data-id', film.id);
+      filmItem.addEventListener('click', () => {
+        displayFilmInfo(film.id);
+      });
+      filmListContainer.appendChild(filmItem);
+    });
+  }
+  
